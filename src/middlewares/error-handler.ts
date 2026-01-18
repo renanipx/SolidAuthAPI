71f4eb52c5e708a1c35a16335d21c88c760cb6ab
renanipx <137443957+renanipx@users.noreply.github.com>
2026-01-18 14:25:58 -0300

    chore: enhance global error handling

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "../errors/app-error";
+import { Prisma } from "@prisma/client";
 
 export function errorHandler(
   error: Error,
@@ -11,6 +12,20 @@ export function errorHandler(
     return res.status(error.statusCode).json({
       message: error.message,
     });
+  }
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    switch (error.code) {
+      case "P2002":
+        return res.status(409).json({
+          message: "Resource already exists",
+        });
+
+      case "P2025":
+        return res.status(404).json({
+          message: "Resource not found",
+        });
+    }
   }
 
   console.error(error);
